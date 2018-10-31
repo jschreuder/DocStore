6a02175bc0cfe3345a264352b93066749b0d5358
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2018-10-31 16:58:39 +0100

    Project setup

---
+++ config/development.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types = 1);
+return [
+    // Database settings
+    'db.dsn' => 'mysql:host=localhost;charset=utf8',
+    'db.dbname' => 'spotdesk_dev',
+    'db.user' => 'root',
+    'db.pass' => '',
+];
